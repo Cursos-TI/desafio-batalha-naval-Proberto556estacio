1526b419baed7f9ad4450888f7781a0b79c8842e
Proberto556estacio <202507269518@alunos.estacio.br>
2025-09-09 15:44:34 -0300

    Terminando código

--- prototipo.c
+++ prototipo.c
@@ -6,18 +6,31 @@ int main() {
     int tabuleiro [10] [10] = {0};
     
     //Posição návio - Horizontal
-    for (int i = 0; i < count; i++)
+    for (int j = 3; j <= 5; j++)  
     {
-        /* code */
+        tabuleiro [2] [j] = 3;
+        printf("Navio na Horizontal: Linha %d, Coluna %d \n", 2, j);
     }
-        
+
+    for (int i = 4; i <= 6; i++)
+    {
+        tabuleiro [i] [7] = 3;
+        printf("Navio na Vertical: Linha %d, Coluna %d \n", i, 7);
+    }
     
+        
+    // Tabuleiro de Batalha Naval - 
 
 
-
-
-
-
+    for (int i = 0; i < 10; i++) //Definição da linha no tabuleiro
+    {
+        for (int j = 0; j < 10; j++)  //Definição da coluna no tabuleiro
+        {
+            printf("%d", tabuleiro [i] [j]);
+        }
+        printf("\n");
+    }
+    
 
 
 

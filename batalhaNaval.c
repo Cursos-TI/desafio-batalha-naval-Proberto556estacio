cc548d135210b5d3347ed0da02b75d5c9756b852
Proberto556estacio <202507269518@alunos.estacio.br>
2025-09-11 19:40:13 -0300

    Atualizando cone

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,9 +36,29 @@ int main() {
         for (int i = 2; i <= 4; i++) {
             int j = 9 - i;
             tabuleiro [i] [j] = 3;
-            printf("Navio 4 na diagonat secundária: linha %d, coluna %d\n", i, j);
+            printf("Navio 4 na diagona secundária: linha %d, coluna %d\n", i, j);
         }
-                
+
+        //Implementando Habilidade Cone
+        int cone[3][5] = {0};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j >= 2 -i && j <= 2 + i);
+                {
+                printf("%\n ");
+                }
+            }
+        }
+        
+        // Mostrando matriz do cone
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                printf("%d ", cone[i][j]);
+        }
+                printf("%d ");
+        }
+        
+               
 
         //Tabuleiro Definido - 0 (zero) representa água e 3 (três) representa partes dos navios.
         printf("\nTabuleiro Batalha Naval\n");
